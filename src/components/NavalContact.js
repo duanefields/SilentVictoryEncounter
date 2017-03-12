0ca9772f044a1b463805ed1cd02b6743eee8a2dd
Duane Fields <duane@Duanes-MacBook-Pro.local>
2017-03-12 17:54:10 -0500

    First stab at card styling

--- src/components/NavalContact.js
+++ src/components/NavalContact.js
@@ -2,24 +2,31 @@ import React from 'react';
 import { inject, observer } from 'mobx-react';
 import classNames from 'classnames';
 import { If } from '../lib'
+import css from './NavalContact.css'
 
 export default inject("appStore")(observer(({appStore, contact}) => {
   const patrol = appStore.patrol;
-  let style = { border: "solid 1px black", margin: "1em"};
 
   return (
-    <div className="card col-md-4" style={style}>
-      <div className="card-block">
-        <div>{contact.type} ({contact.entryType})</div>
-        <If cond={contact.image}>
-          <img src={contact.image} className="img-fluid"/>
-        </If>
-        <div>{contact.name}</div>
-        <div>{contact.tonnage.toLocaleString()} tons</div>
-        <div>{contact.damageRequired} damage</div>
-        <If cond={contact.entryType === "Escort"}>
-          <div>({contact.quality} Crew)</div>
-        </If>
+    <div className="card col-md-4">
+      <div className={"card-block " + css.card}>
+
+        <div className={css.sky}>
+          <div className={css.name}>{contact.name}</div>
+          <If cond={contact.image}>
+            <img src={contact.image} className={css.image + " img-fluid"}/>
+          </If>
+        </div>
+
+        <div className={css.water}>
+          <div className={css.damage}>{contact.damageRequired}</div>
+          <div className={css.type}>{contact.type} ({contact.entryType})</div>
+          <div className={css.tonnage}>{contact.tonnage.toLocaleString()} tons</div>
+          <If cond={contact.entryType === "Escort"}>
+            <div>({contact.quality} Crew)</div>
+          </If>
+        </div>
+
       </div>
     </div>
   );
