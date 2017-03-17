f88d03129beabcbb00c786f3ba574bf6c698d129
Duane Fields <duane@duanefields.com>
2017-03-17 07:00:31 -0500

    Center button on patrol end

--- src/components/PatrolComplete.js
+++ src/components/PatrolComplete.js
@@ -7,9 +7,16 @@ export default inject("appStore")(observer(({appStore}) => {
     <div className="text-center">
       <h1>Patrol Complete</h1>
       <h2>Welcome back to {patrol.base}</h2>
-      <button className="btn btn-primary" onClick={patrol.newPatrol}>
-        Begin New Patrol
-      </button>
+
+      <div style={ {height: '5em'} }>&nbsp;</div>
+
+      <div className="text-center row">
+        <div className="offset-2 col-8" style={ {height: '5em'} }>
+          <button className="btn btn-primary btn-block" onClick={patrol.newPatrol}>
+            Begin New Patrol
+          </button>
+        </div>
+      </div>
     </div>
   );
 }))
