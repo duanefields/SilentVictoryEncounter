b3014cf0a108b78b27a105e2a984bbff369885a7
Duane Fields <duane@duanefields.com>
2017-03-17 20:12:54 -0500

    fix aircraft encounter rendering

--- src/components/Aircraft.js
+++ src/components/Aircraft.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
 import { If } from '../lib'
+import css from './Aircraft.css'
 
 export default inject("appStore")(observer(({appStore, contact}) => {
   const patrol = appStore.patrol;
@@ -9,10 +10,12 @@ export default inject("appStore")(observer(({appStore, contact}) => {
   return (
     <div className="card col-md-2" style={style}>
       <div className="card-block">
-        <div>Aircraft</div>
-        <div>{contact.name}</div>
-        <div>"{contact.codeName}"</div>
-        <div>({contact.quality} Crew)</div>
+        <div className={css.sky}>
+          <div className={css.name}>{contact.name}</div>
+          <div>"{contact.codeName}"</div>
+          <img src={contact.image} className={css.image + " img-fluid"}/>
+          <div>({contact.quality} Crew)</div>
+        </div>
       </div>
 
       <div>
